a8e8c4eddacbe8d823c83304aea13d0da7c6508f
marigameo <mariappangameo@gmail.com>
2019-12-06 13:51:10 +0530

    little changes

--- js/delegation.js
+++ js/delegation.js
@@ -6,3 +6,11 @@ document.querySelector('#components')
         '_blank'
     )
 })
+
+document.querySelector('#form')
+.addEventListener('keyup',(e)=>{
+    console.log(e.target.dataset);
+    if(e.target.dataset.uppercase!=undefined){
+        e.target.value = e.target.value.toUpperCase();
+    }
+})
